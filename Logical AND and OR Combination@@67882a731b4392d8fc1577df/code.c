ca2860757587e85e9505682a1d44c0391136841f
Swastik Jindal <88715431+swastikxjindal@users.noreply.github.com>
2025-01-19 14:30:19 +0530

    Solution to Logical AND and OR Combination

---
+++ Logical AND and OR Combination@@67882a731b4392d8fc1577df/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int a,b;
+    scanf("%d",a);
+    scanf("%d",b);
+    if (x>0 && b<0) || (a == b == 0){
+        printf("True");
+    }
+    else{
+        printf("False")
+    }
+}
