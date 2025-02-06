437faf6dd3efd5c788e308c97d31a4549f9bf34d
Swastik Jindal <88715431+swastikxjindal@users.noreply.github.com>
2025-02-06 18:35:11 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    char c;
+    scanf("%c",&c);
+    switch(c){
+        case 'A'{
+            printf("Excellent");
+        }
+        break;
+        case 'B'{
+            printf("Good");
+        }
+        break;case 'C'{
+            printf("Average");
+        }
+        break;case 'D'{
+            printf("Below Average");
+        }
+        break;case 'F'{
+            printf("Fail");
+        }
+        break;
+        default:
+        printf("Invalid grade")
+    }
+    return 0;
+}
